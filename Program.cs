aaf62cf2f39830ad659eaea3ac4202a0f1601bd7
Carmen Jisoo <76640113+Hellocarmenn@users.noreply.github.com>
2022-03-26 20:43:58 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace INTERFACES
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+
+        }
+        int Propiedad1{
+            get;
+            set;
+
+        }
+    }
+}
+//Definicion de interfaz 
+public interface IEjemplo{
+    int Propiedad1{
+        get;
+        set;
+    }
+    void Metodo1();
+
+
+}
